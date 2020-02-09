31b881d626e32b3e84f11677fe81ce2a99e7ceb9
Gaurav Gogia <gauravgogia@light.local>
2020-02-09 09:28:22 +0530

    lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,151 @@
+package lexer
+
+import (
+	"mutant/token"
+)
+
+// Lexer is the data structure for our lexer
+// It performs lexical analysis and tokenizes code
+type Lexer struct {
+	input        string
+	position     int // current character index
+	readPosition int // next character index
+	ch           byte
+}
+
+// New function initializes our lexer, takes input as a string
+// that input is the source code
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// NextToken method makes use of lexer data structure
+// Uses switch cases to identify whether a certain character
+// in source code is legal or not. Mutant language only
+// supports ascii characters
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhiteSpace()
+
+	switch l.ch {
+	case '=':
+		if l.peekChar() == '=' {
+			ch := string(l.ch)
+			l.readChar()
+			tok = token.Token{Type: token.EQUALITY, Literal: ch + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '/':
+		tok = newToken(token.FSLASH, l.ch)
+	case '\\':
+		tok = newToken(token.FSLASH, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := string(l.ch)
+			l.readChar()
+			tok = token.Token{Type: token.INEQUALITY, Literal: ch + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case 0:
+		tok = newToken(token.EOF, l.ch)
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.ch)
+	}
+
+	l.readChar()
+
+	return tok
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	var tok token.Token
+
+	tok.Type = tokenType
+	tok.Literal = string(ch)
+
+	return tok
+}
+
+func isLetter(ch byte) bool {
+	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return ('0' <= ch && ch <= '9')
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for (l.ch == ' ') || (l.ch == '\n') || (l.ch == '\t') || (l.ch == '\r') {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+	return l.input[l.readPosition]
+}
---
+++ token/token.go
@@ -0,0 +1,68 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + Literals
+	// ex: add, foobar, x, y, ....
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN     = "="
+	PLUS       = "+"
+	MINUS      = "-"
+	ASTERISK   = "*"
+	FSLASH     = "/"
+	BSLASH     = "\\"
+	LT         = "<"
+	GT         = ">"
+	BANG       = "!"
+	EQUALITY   = "=="
+	INEQUALITY = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+// LookupIdent function takes in an identifier(string)
+// and then returns whether that identifier is a keyword
+// or a user defined identifier
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
