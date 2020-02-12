f63c60ee17c84fcf2b4b7a2380eef285f58c3274
Gaurav Gogia <gauravgogia@light.local>
2020-02-13 01:48:05 +0530

    infixParser structure added

---
+++ ast/infixExpression.go
@@ -0,0 +1,15 @@
+package ast
+
+import (
+	"mutant/token"
+)
+
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
