993a4930a3476ba0e4fdea08ae42ce30dec27a05
Gaurav Gogia <gauravgogia@light.local>
2020-02-16 22:22:24 +0530

    eval tests restructure

---
+++ evaluator/eval_conds.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"mutant/lexer"
+	"mutant/object"
+	"mutant/parser"
+	"testing"
+)
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,9 +1,6 @@
 package evaluator
 
 import (
-	"mutant/lexer"
-	"mutant/object"
-	"mutant/parser"
 	"testing"
 )
 
@@ -17,25 +14,4 @@ func TestEvalIntegerExpression(t *testing.T) {
 		evaluated := testEval(tt.input)
 		testIntegerObject(t, evaluated, tt.expected)
 	}
-}
-
-func testEval(input string) object.Object {
-	l := lexer.New(input)
-	p := parser.New(l)
-	program := p.ParseProgram()
-	return Eval(program)
-}
-
-func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
-	result, ok := obj.(*object.Integer)
-	if !ok {
-		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
-		return false
-	}
-
-	if result.Value != expected {
-		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
-		return false
-	}
-	return true
 }
