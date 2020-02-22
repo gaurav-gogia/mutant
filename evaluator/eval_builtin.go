cfbbd76415b282328686a44506320cc9910598b8
Gaurav Gogia <gauravgogia@light.local>
2020-02-22 16:10:31 +0530

    len function added

--- evaluator/eval_builtin.go
+++ evaluator/eval_builtin.go
@@ -7,7 +7,15 @@ import (
 var builtins = map[string]*object.BuiltIn{
 	"len": &object.BuiltIn{
 		Fn: func(args ...object.Object) object.Object {
-			return NULL
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
 		},
 	},
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -252,7 +252,7 @@ func TestBuiltInFunctions(t *testing.T) {
 		{`len("four")`, 4},
 		{`len("hello world")`, 11},
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
-		{`len("one", "two")`, "wrong number of arguments.arguments. got=2, want=1"},
+		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
