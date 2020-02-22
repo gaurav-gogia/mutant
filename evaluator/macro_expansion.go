79cc0a910f86e30cf3d788cf9e7f99a31baf5ed2
Gaurav Gogia <gauravgogia@light.local>
2020-02-22 22:06:29 +0530

    macro expansion added

--- evaluator/macro_expansion.go
+++ evaluator/macro_expansion.go
@@ -50,3 +50,68 @@ func addMacro(stmt ast.Statement, env *object.Environment) {
 
 	env.Set(letStatement.Name.Value, macro)
 }
+
+func ExpandMacros(program ast.Node, env *object.Environment) ast.Node {
+	return ast.Modify(program, func(node ast.Node) ast.Node {
+		callExpression, ok := node.(*ast.CallExpression)
+		if !ok {
+			return node
+		}
+
+		macro, ok := isMacroCall(callExpression, env)
+		if !ok {
+			return node
+		}
+
+		args := quoteArgs(callExpression)
+		evalEnv := extendMacroEnv(macro, args)
+
+		evaluated := Eval(macro.Body, evalEnv)
+
+		quote, ok := evaluated.(*object.Quote)
+		if !ok {
+			panic("we only support returning AST-nodes from macros")
+		}
+
+		return quote.Node
+	})
+}
+
+func isMacroCall(exp *ast.CallExpression, env *object.Environment) (*object.Macro, bool) {
+	identifier, ok := exp.Function.(*ast.Identifier)
+	if !ok {
+		return nil, false
+	}
+
+	obj, ok := env.Get(identifier.Value)
+	if !ok {
+		return nil, false
+	}
+
+	macro, ok := obj.(*object.Macro)
+	if !ok {
+		return nil, false
+	}
+
+	return macro, true
+}
+
+func quoteArgs(exp *ast.CallExpression) []*object.Quote {
+	args := []*object.Quote{}
+
+	for _, a := range exp.Arguments {
+		args = append(args, &object.Quote{Node: a})
+	}
+
+	return args
+}
+
+func extendMacroEnv(macro *object.Macro, args []*object.Quote) *object.Environment {
+	extended := object.NewEnclosedEnvironement(macro.Env)
+
+	for paramIdx, param := range macro.Parameters {
+		extended.Set(param.Value, args[paramIdx])
+	}
+
+	return extended
+}
--- evaluator/macro_expansion_test.go
+++ evaluator/macro_expansion_test.go
@@ -64,6 +64,44 @@ func TestDefineMacros(t *testing.T) {
 	}
 }
 
+func TestExpandMacros(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			`
+            let infixExpression = macro() { quote(1 + 2); };
+
+            infixExpression();
+            `,
+			`(1 + 2)`,
+		},
+		{
+			`
+            let reverse = macro(a, b) { quote(unquote(b) - unquote(a)); };
+
+            reverse(2 + 2, 10 - 5);
+            `,
+			`(10 - 5) - (2 + 2)`,
+		},
+	}
+
+	for _, tt := range tests {
+		expected := testParseProgram(tt.expected)
+		program := testParseProgram(tt.input)
+
+		env := object.NewEnvironment()
+		DefineMacros(program, env)
+		expanded := ExpandMacros(program, env)
+
+		if expanded.String() != expected.String() {
+			t.Errorf("not equal. want=%q, got=%q",
+				expected.String(), expanded.String())
+		}
+	}
+}
+
 func testParseProgram(input string) *ast.Program {
 	l := lexer.New(input)
 	p := parser.New(l)
