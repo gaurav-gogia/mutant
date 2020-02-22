eb02577dd9c0b67638312623863cf83fabd82b2b
Gaurav Gogia <gauravgogia@light.local>
2020-02-22 18:24:07 +0530

    hash key implemented in object

---
+++ object/hashObj.go
@@ -0,0 +1,28 @@
+package object
+
+import "hash/fnv"
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
