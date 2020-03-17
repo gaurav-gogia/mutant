b14584e984e01eee77312abb9c7c570145773e97
Gaurav Gogia <gauravgogia@light.local>
2020-03-17 07:41:03 +0530

    laid down foundations for security

---
+++ runner/runner.go
@@ -0,0 +1,15 @@
+package runner
+
+func Start() {
+	// read file
+	// check file type
+	// run
+	//	--> verify signatures
+	//	--> verify checksum
+	//	--> decrypt
+	//	--> send bytecode to vm
+	//	--> -->	decrypt every bytecode and run
+}
+
+func run()     {}
+func compile() {}
