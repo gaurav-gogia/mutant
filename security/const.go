3b78c6b2a702976e8e6627f3cd0e35cc430696b4
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2021-03-01 01:20:57 +0530

    file verification signatures

--- security/const.go
+++ security/const.go
@@ -1,1 +1,14 @@
 package security
+
+import "errors"
+
+// Header and footer constant strings for file identification
+const (
+	HEADER    = "MUT"
+	FOOTER    = "ANT"
+	ENCSIG    = "MUTANT"
+	SEPERATOR = "|"
+)
+
+// ErrWrongSignature error is returned if signature doesn't match
+var ErrWrongSignature = errors.New("Wrong signature, compromised or wrong file")
